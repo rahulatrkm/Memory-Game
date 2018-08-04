3ee01a1bca29996d01ff0c192c094893fae89c1d
Rahul <rahulatrkm@gmail.com>
2018-08-04 22:48:51 +0530

    Added one and two card functionality
    After clicking one the color changes to the desired one and after match the colour changes to the final color.
    
    But there are many things yet to be implemented and also debugging on the implementation right now

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
  * Create a list that holds all of your cards
  */
 
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +36,53 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+let t = 0
+let temp, temp1, temp2 = 0;
+
+let clickHandler = function (event) {
+    let first = event.target;
+    if (temp2) {
+        console.log(temp2);
+        console.log(first);
+    }
+    
+    if (t === 0) {
+        temp2 = first;
+        temp = first.firstChild.nextSibling.className;
+        displayOne(first);
+    } else {
+        temp1 = first.firstChild.nextSibling.className;
+        if (temp === temp1) {
+            displayTwo(first, temp2);
+        }
+        else {
+            hideOne(temp2);
+        }
+    }
+}
+
+const card = document.querySelector(".deck");
+card.addEventListener('click', clickHandler, false);
+
+function displayOne(first) {
+    first.classList.add("open");
+    first.classList.add("show");
+    first.removeEventListener('click', clickHandler);
+    t = 1;
+}
+
+function hideOne(first) {
+    first.classList.remove("open");
+    first.classList.remove("show");
+    first.addEventListener('click', clickHandler);
+    t = 0
+}
+
+function displayTwo(first, second) {
+    first.classList.remove("open");
+    first.classList.remove("show");
+    first.removeEventListener('click', clickHandler);
+    first.classList.add("match");
+    second.classList.add("match");
+    t = 0;
+}
